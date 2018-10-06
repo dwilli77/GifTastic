f6129b3b49eac88b39e409ab8deca86f529c93d7
Derek W <41767875+dwilli77@users.noreply.github.com>
2018-10-06 16:22:04 -0400

    needs styling

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 // API KEY = yOEsL4XsJC1T3awMVcC8cx8B4v5wacvv
 
 let topics = ["dog", "cat", "otter", "chicken", "donkey", "squirrel", "bear", "monkey", "elephant", "mouse", "rabbit", "hedgehog", "camel", "lion", "kangaroo"];
-
-let topic = "";
-
+// iterates over the topics array and appends buttons to the DOM
 function buttonBuilder() {
     $('.button-area').empty();
     for(let i = 0; i<topics.length;i++){
-        let newButton = $('<button class="topic-button" value="' + topics[i] + '">' + topics[i] + '</button>');
+        let newButton = $('<button>').addClass('category-button').text(topics[i]);
+        newButton.data('name', topics[i]);
         $('.button-area').append(newButton);
     };
 }
-buttonBuilder();
-
-// clicking on a topic button populates the page with Gifs
-$('.topic-button').on('click', function(){
-    // reassings the topic in the queryURL
-    topic = $(this).attr('value');
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=yOEsL4XsJC1T3awMVcC8cx8B4v5wacvv$limit=10"
-    // queries that URL and builds gifs based on response
+// click even on the -Add!- button to add a category
+$('#add-category').on('click', function(event){
+    event.preventDefault();
+    let cat = $('#category-input').val().trim();
+    if(cat){ // makes sure the user doesn't add blank buttons
+        topics.push(cat);
+        buttonBuilder();
+        $('#category-input').val("");
+    }else{
+        return false;
+    }
+});
+// clicking a gif will play/pause it
+function toggleGif(){
+    let state = $(this).data('state');
+    if(state === 'still'){
+        $(this).attr('src', $(this).data('active'));
+        $(this).data('state', 'active');
+    }else{
+        $(this).attr('src', $(this).data('still'));
+        $(this).data('state', 'still');
+    };
+}
+// displays the gif on the page when called (by clicking a category button)
+function displayGifs(){
+    let category = $(this).data('name');
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=yOEsL4XsJC1T3awMVcC8cx8B4v5wacvv&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -29,28 +47,19 @@ $('.topic-button').on('click', function(){
         for(let i = 0; i < dataArray.length;i++){
             let gifDiv = $('<div class="gif-card">');
             let gifTitle = $('<div class="git-title">'+ response.data[i].title+'</div>');
-            let newGif = $('<img class="gif" src="' + response.data[i].images.fixed_height_still.url +'" data-pos="'+ i +'">');
             let gifRating = $('<div class="gif-rating">Rating: '+ response.data[i].rating.toUpperCase()+'</div>');
-            gifDiv.append(gifTitle, newGif, gifRating);
+            let newGif = $('<img class="gif">');
+            newGif.attr('src', response.data[i].images.fixed_height_still.url);
+            newGif.attr('data-state', 'still');
+            newGif.attr('data-still', response.data[i].images.fixed_height_still.url);
+            newGif.attr('data-active', response.data[i].images.fixed_height.url);
+            gifDiv.append(gifTitle, gifRating, newGif);
             $('.gif-area').append(gifDiv);
         };
-        // clicking each gif will let it play until clicked again
-        $('.gif').on('click', function() {
-            let source = $(this).attr('src');
-            let position = $(this).data('pos');
-            if (source === response.data[position].images.fixed_height_still.url) {
-                $(this).attr('src', response.data[position].images.fixed_height.url);
-            }else if(source === response.data[position].images.fixed_height.url){
-                $(this).attr('src', response.data[position].images.fixed_height_still.url);
-            };
-        });
     });
-});
-
-$.ajax({
-    url:"https://api.giphy.com/v1/gifs/search?q=dog&api_key=yOEsL4XsJC1T3awMVcC8cx8B4v5wacvv$limit=10",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
-});
-
+}
+// click events for submit button and each gif
+$(document).on('click', '.category-button', displayGifs);
+$(document).on('click', '.gif', toggleGif);
+//builds preset buttons
+buttonBuilder();
